4cfc4f4dc8b6b2cf01715d8efabc54de9002727f
TARAKANTA <tmohapatra111@gmail.com>
2025-11-28 13:50:30 +0530

    initial commit 28112025

--- API/Controllers/MemberController.cs
+++ API/Controllers/MemberController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.Entites;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -12,16 +13,16 @@ namespace API.Controllers
     public class MembersController(AppDbContext context) : ControllerBase
     {
         [HttpGet]
-        public ActionResult <IReadOnlyList<AppUser>> GetMembers()
+        public async Task<ActionResult <IReadOnlyList<AppUser>>> GetMembers()
         {
-            var Members= context.Users.ToList();
+            var Members= await context.Users.ToListAsync();
             return Members;
         }
 
         [HttpGet("{id}")] // localhost:5000/api/Members/tdm-id
-        public ActionResult<AppUser>GeMember(string Id)
+        public async Task<ActionResult<AppUser>>GeMember(string Id)
         {
-            var Member= context.Users.Find(Id);
+            var Member= await context.Users.FindAsync(Id);
             if (Member == null) return NotFound();
             
              return Member;
--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97e319bc52c1f2915ad214148483979d7c092f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497e0809d30f23fdc775fdfcd3f76ec288376449")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
